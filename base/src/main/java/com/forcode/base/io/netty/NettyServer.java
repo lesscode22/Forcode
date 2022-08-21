6adc45b452b0b8716acde83c917ca1c453f4f3e8
tangjiu <tangjiu@p-pass.com>
2022-08-21 12:34:06 +0800

    netty demo

---
+++ base/src/main/java/com/forcode/base/io/netty/NettyServer.java
@@ -0,0 +1,55 @@
+package com.forcode.base.io.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.concurrent.DefaultThreadFactory;
+
+public class NettyServer {
+
+    public static void main(String[] args) throws InterruptedException {
+        // 创建两个线程组 boosGroup、workerGroup
+        // bossGroup: 用于监听客户端连接, 专门负责与客户端创建连接, 并把连接注册到workerGroup的Selector中
+        // workerGroup: 用于处理每一个连接发生的读写事件
+        EventLoopGroup bossGroup = new NioEventLoopGroup(0, new DefaultThreadFactory("boss"));
+        EventLoopGroup workerGroup = new NioEventLoopGroup(0, new DefaultThreadFactory("worker"));
+        try {
+            // 创建服务端的启动对象，设置参数
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            // 设置两个线程组boosGroup和workerGroup
+            bootstrap.group(bossGroup, workerGroup)
+                    // 设置服务端通道实现类型
+                    .channel(NioServerSocketChannel.class)
+                    // 设置线程队列得到连接个数
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    // 设置保持活动连接状态
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    // 使用匿名内部类的形式初始化通道对象
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            // 给pipeline管道设置处理器
+                            socketChannel.pipeline().addLast("customSH", new CustomServerHandler());
+
+                            // 配置日志打印
+                            socketChannel.pipeline().addLast("loggingH", new LoggingHandler(LogLevel.INFO));
+                        }
+                    });
+            System.out.println("java技术爱好者的服务端已经准备就绪...");
+            // 绑定端口号，启动服务端
+            ChannelFuture channelFuture = bootstrap.bind(6666).sync();
+            // 对关闭通道进行监听
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
